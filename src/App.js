f34e6fcb8b64f3f7dfb58994b1b745fdaf63790f
Victor Caruso <vic.caruso@gmail.com>
2022-02-24 09:19:12 -0800

    wrapped and some styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import data from './data';
 function App() {
   return (
     <div className="App">
-      <Grid
-        data={data}
-        search={true}
-        pagination={{
-          enabled: true,
-          limit: 20,
-        }}
-      />
+      <div className='data-table'>
+        <h1>Data Table</h1>
+        <Grid
+          data={data}
+          search={true}
+          pagination={{
+            enabled: true,
+            limit: 20,
+          }}
+        />
+      </div>
+      <div className='fancy-graphs'>
+        <h1>Fancy Graphs</h1>
+      </div>
     </div>
   );
 }
